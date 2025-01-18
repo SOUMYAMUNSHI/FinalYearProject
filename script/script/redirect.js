ccd163cec00c422e5b8e322f1b1e881461350dd9
Soumya <Soumyamunsi@gmail.com>
2025-01-18 22:20:22 +0530

    The sign up page is created

---
+++ script/script/redirect.js
@@ -0,0 +1,11 @@
+document.getElementById("teacher_signup").addEventListener("click", () => {
+  window.location.replace(
+    "http://127.0.0.1:5500/components/teacher_registration.html"
+  ); //redirect to teacher sign up page
+});
+
+document.getElementById("student_signup").addEventListener("click", () => {
+  window.location.replace(
+    "http://127.0.0.1:5500/components/student_registration.html"
+  ); //redirect to student sign up page
+});
