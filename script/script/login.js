0f863cc092fe76b8e65fa5132036d0072994e7df
Soumya <Soumyamunsi@gmail.com>
2025-01-19 10:58:49 +0530

    Page redirect is done using js

---
+++ script/script/login.js
@@ -0,0 +1,7 @@
+$("#student_login").click(function () {
+  window.location.replace("http://127.0.0.1:5500/student/student_home.html");
+});
+
+$("#teacher_login").click(function () {
+  window.location.replace("http://127.0.0.1:5500/teacher/teacher_home.html");
+});
