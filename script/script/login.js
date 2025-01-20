cfa71c4e5f6966eccb58da3bc1748a335eb19f6e
Soumya <Soumyamunsi@gmail.com>
2025-01-20 18:34:33 +0530

    Additional updates

--- script/script/login.js
+++ script/script/login.js
@@ -1,7 +1,10 @@
+let port = window.location.port;
+let host = window.location.hostname;
+
 $("#student_login").click(function () {
-  window.location.replace("http://127.0.0.1:5500/student/student_home.html");
+  window.location.replace(`http://${host}:${port}/student/student_home.html`);
 });
 
 $("#teacher_login").click(function () {
-  window.location.replace("http://127.0.0.1:5500/teacher/teacher_home.html");
+  window.location.replace(`http://${host}:${port}/teacher/teacher_home.html`);
 });
--- script/script/redirect.js
+++ script/script/redirect.js
@@ -1,8 +1,10 @@
+let port = window.location.port;
+let host = window.location.hostname;
+
 document.getElementById("teacher_signup").addEventListener("click", () => {
-  window.location.href =
-    "http://127.0.0.1:5500/components/teacher_registration.html"; //redirect to teacher sign up page
+  window.location.href = `http://${host}:${port}/components/teacher_registration.html`; //redirect to teacher sign up page
 });
 
 document.getElementById("student_signup").addEventListener("click", () => {
-  window.location.href = "http://127.0.0.1:5500/components/student_registration.html"; //redirect to student sign up page
+  window.location.href = `http://${host}:${port}/components/student_registration.html`; //redirect to student sign up page
 });
