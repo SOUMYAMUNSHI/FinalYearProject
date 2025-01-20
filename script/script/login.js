27f632feea3e9447986e7f47924de5b066ab8c95
Soumya <Soumyamunsi@gmail.com>
2025-01-20 19:15:12 +0530

    The url is updated

--- script/script/login.js
+++ script/script/login.js
@@ -2,9 +2,9 @@ let port = window.location.port;
 let host = window.location.hostname;
 
 $("#student_login").click(function () {
-  window.location.replace(`http://${host}:${port}/student/student_home.html`);
+  window.location.href = `http://${host}:${port}/student/student_home.html`;
 });
 
 $("#teacher_login").click(function () {
-  window.location.replace(`http://${host}:${port}/teacher/teacher_home.html`);
+  window.location.href = `http://${host}:${port}/teacher/teacher_home.html`;
 });
